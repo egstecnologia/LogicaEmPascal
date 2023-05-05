c91f75b755cf0547ec67cc242b72be681dffa3bb
egstecnologia <egstecnologia@gmail.com>
2023-05-04 21:59:42 -0300

    Aumento de Salario

---
+++ AumentoSalario/AumentoSalario.dpr
@@ -0,0 +1,27 @@
+program AumentoSalario;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+var
+  SalarioAnterior : Real;
+  Aumento : Real;
+  SalarioNovo : Real;
+begin
+  write ('Digite o salario atual: R$ ');
+  readln (SalarioAnterior);
+
+  write ('Qual o percentual de aumento: ');
+  readln (Aumento);
+
+  SalarioNovo := SalarioAnterior + SalarioAnterior * Aumento /100;
+
+  write ('Salario reajustado é: R$ ', SalarioNovo:10:2);
+  readln;
+
+
+end.
